dcbbc47e23c2ed3c0914713c2497b14063dc6691
MOZGIII <mike-n@narod.ru>
2022-02-16 17:18:35 +0300

    Add a test for case transformation

--- src/utils/formatEnvKeyWithPrefix.test.ts
+++ src/utils/formatEnvKeyWithPrefix.test.ts
@@ -7,3 +7,8 @@ it("works with empty prefix", () => {
 it("works with non-empty prefix", () => {
   expect(formatEnvKeyWithPrefix("test_")("key")).toBe("TEST_KEY");
 });
+
+it("convers multiple words", () => {
+  expect(formatEnvKeyWithPrefix("test_")("myKey")).toBe("TEST_MY_KEY");
+  expect(formatEnvKeyWithPrefix("test_")("my_key")).toBe("TEST_MY_KEY");
+});
